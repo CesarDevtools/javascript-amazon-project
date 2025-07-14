a4d4d81ae0ecbbc44d1bb382f648cfb8e578a2d9
Cesar <cessargalan@gmail.com>
2025-07-14 14:05:58 +0100

    Primeros tests con jasmine

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () => {
+    it('convierte centavos a dolares', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+
+it('funciona con 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('redondea al centavo más cercano', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
